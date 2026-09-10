cd2910a6c01df3957ec7df4441f82b257f9d2b85
sushant9sushant <lenswork.imagination@gmail.com>
2026-09-10 16:23:07 +0530

    adding P009_Easy_StrobogrammaticNumber

--- dsawork/src/twospointers/P009_Easy_StrobogrammaticNumber.java
+++ dsawork/src/twospointers/P009_Easy_StrobogrammaticNumber.java
@@ -1,5 +1,8 @@
 package twospointers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P009_Easy_StrobogrammaticNumber {
 
 
@@ -23,7 +26,29 @@ public class P009_Easy_StrobogrammaticNumber {
 
     private static boolean isStrobogrammatic(String num) {
 
+        Map<Character, Character> map = new HashMap<>();
 
+        map.put('0','0');
+        map.put('1','1');
+        map.put('8','8');
+        map.put('6','9');
+        map.put('9','6');
+
+        int left = 0;
+        int right = num.length() - 1;
+
+        while(left <= right ) {
+
+            if(!map.containsKey(num.charAt(left)) || map.get(num.charAt(left)) != num.charAt(right) ) {
+
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
