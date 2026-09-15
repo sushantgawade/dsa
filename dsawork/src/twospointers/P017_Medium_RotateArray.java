cc92997fc702f0ffb624dc816a996241a2dc7509
sushant9sushant <lenswork.imagination@gmail.com>
2026-09-15 20:04:07 +0530

    adding P017_Medium_RotateArray

--- dsawork/src/twospointers/P017_Medium_RotateArray.java
+++ dsawork/src/twospointers/P017_Medium_RotateArray.java
@@ -49,6 +49,15 @@ public class P017_Medium_RotateArray {
 
     public static void reverse(int[] nums, int left, int right) {
 
+        while (left < right) {
+
+            int temp =  nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 
     // Determine the total number of elements in the array 'n'.
@@ -67,6 +76,13 @@ public class P017_Medium_RotateArray {
 
     public static void rotate(int[] nums, int k) {
 
+        int n = nums.length;
+
+        k = k % n;
+
+        reverse(nums, 0, n-1);
+        reverse(nums,0, k-1);
+        reverse(nums,k, n-1);
     }
 
 
