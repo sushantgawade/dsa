dfd96ebb9824b8c9bc351dd402810a03a5086e9f
sushant9sushant <lenswork.imagination@gmail.com>
2026-09-16 17:42:54 +0530

    adding P018_Medium_NextPermutation

--- dsawork/src/twospointers/P018_Medium_NextPermutation.java
+++ dsawork/src/twospointers/P018_Medium_NextPermutation.java
@@ -85,8 +85,46 @@ Let’s look at the following illustration to get a better understanding of the 
 
 */
 
-    private void nextPermutation(int[] testCase) {
+    private void swap(int nums[], int i, int j ) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] =  temp;
     }
+
+    private void reverse(int[] nums , int start, int end) {
+
+        while(start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void nextPermutation(int[] nums) {
+
+        int i  = nums.length - 2;
+
+        while(i >= 0 && nums[i+1] <= nums[i]) {
+            i--;
+        }
+
+        if( i >= 0) {
+
+            int j = nums.length -1;
+
+            while ( nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+
+    }
+
 
 
     public static void main(String[] args) {
