78db6fd494800a6dc34856fe1fadd9a9793fbd53
sushant9sushant <lenswork.imagination@gmail.com>
2026-09-21 19:46:59 +0530

    adding P024_Medium_MoveZeroes

--- dsawork/src/twospointers/P024_Medium_MoveZeroes.java
+++ dsawork/src/twospointers/P024_Medium_MoveZeroes.java
@@ -48,7 +48,20 @@ Once right has traversed the entire array, all non-zero elements occupy indices 
     }
      */
 
-    private void moveZeroes(int[] testCase) {
+    private void moveZeroes(int[] nums) {
+        int left = 0;
+
+        for(int right = 0; right < nums.length; right++) {
+
+            if(nums[right] != 0) {
+
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+            }
+        }
     }
 
     public static void main(String[] args) {
